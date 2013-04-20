bb640031d4f8327208dca75c6e5e59ae305d2b6e
Scott Schaffter <scottschaffter@gmail.com>
2013-04-20 14:17:20 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tedxrosslyn::Application.config.secret_token = 'cfdee2ac01139d22cb46ad6267e1b2a2a1cf1c117e49514dc3f06823f38d1439bd7b24e07a0abde31f698016c9205e3e245b948dbce9743282796d61a2959b2e'
